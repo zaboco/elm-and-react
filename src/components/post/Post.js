aec42dc89a3f5cc970d310d14aae0ec3ab361679
Bogdan Zaharia <zaboco@gmail.com>
2017-04-07 02:22:09 +0300

    add post votes ui

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -14,6 +14,11 @@ const Post = ({ id, title }: PostParams) => (
   <div className={postClass(id === 1)}>
     <span className="Post-id">{id}</span>
     <span className="Post-title">{title}</span>
+    <div className="Post-votes">
+      <button>-</button>
+      <span>5</span>
+      <button>+</button>
+    </div>
   </div>
 );
 
