53a404f44bf59e013cd37fc8774d0756b520aa43
Bogdan Zaharia <zaboco@gmail.com>
2017-04-07 23:48:13 +0300

    set default votes to 0

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -6,21 +6,26 @@ import './Post.css';
 type PostParams = {
   id: number,
   title: string,
+  votes?: number,
 };
 
 const postClass = selected => selected ? 'Post selected' : 'Post';
 
-const Post = ({ id, title }: PostParams) => (
+const Post = ({ id, title, votes }: PostParams) => (
   <div className={postClass(id === 1)}>
     <span className="Post-id">{id}</span>
     <span className="Post-title">{title}</span>
     <div className="Post-votes">
       <button>-</button>
-      <span>5</span>
+      <span>{votes}</span>
       <button>+</button>
     </div>
   </div>
 );
+
+Post.defaultProps = {
+  votes: 0,
+};
 
 export type { PostParams };
 export default Post;
