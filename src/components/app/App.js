2b92a4b7de63674b2eecd3bc6dd9844010e5ea31
Bogdan Zaharia <zaboco@gmail.com>
2017-04-08 18:36:54 +0300

    add pseudo-routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,17 +8,55 @@ import logo from './logo.svg';
 import PostListContainer from '../PostListContainer';
 import './App.css';
 
+const PAGES = ['React & Redux', 'Elm'];
+const [REACT_REDUX, ELM] = PAGES;
+
+const navItemClass = selected => selected ? 'App-nav-item selected' : 'App-nav-item';
+
+const Nav = ({ pages, selectedPage, onChange }) => (
+  <div className="App-nav">
+    {pages.map(page => (
+      <div
+        key={page}
+        className={navItemClass(page === selectedPage)}
+        onClick={() => onChange(page)}
+      >
+        {page}
+      </div>
+    ))}
+  </div>
+);
+
+const AppBody = ({ page }) => {
+  switch (page) {
+    case REACT_REDUX:
+      return <PostListContainer />;
+    case ELM:
+      return <Elm src={Posts} />;
+    default:
+      return <div>Not Found</div>;
+  }
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: ELM,
+    };
+  }
   render() {
+    const { page } = this.state;
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Posts from React</h2>
+          <h2>Posts from {page}</h2>
         </div>
+        <Nav pages={PAGES} onChange={page => this.setState({ page })} selectedPage={page} />
         <div className="App-body">
-          <PostListContainer />
-          <Elm src={Posts} />
+          <AppBody page={page} />
         </div>
       </div>
     );
